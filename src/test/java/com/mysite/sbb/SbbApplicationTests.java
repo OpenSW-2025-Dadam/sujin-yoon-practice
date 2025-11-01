65ebf4adf0e1d830f66ea844780b86c093f84f15
s90d221 <sujin90d@gmail.com>
2025-11-02 07:23:17 +0900

    test: Add assertions to verify question count and subject

--- src/test/java/com/mysite/sbb/SbbApplicationTests.java
+++ src/test/java/com/mysite/sbb/SbbApplicationTests.java
@@ -1,5 +1,9 @@
 package com.mysite.sbb;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.util.List;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -12,19 +16,11 @@ class SbbApplicationTests {
     private QuestionRepository questionRepository;
 
     @Test
-    void contextLoads() {
-        Question q1 = new Question();
-        q1.setSubject("sbb가 무엇인가요?");
-        q1.setContent("sbb에 대해서 알고 싶습니다.");
-        q1.setCreateDate(LocalDateTime.now());
-        this.questionRepository.save(q1);
+    void testJpa() {
+        List<Question> all = this.questionRepository.findAll();
+        assertEquals(2, all.size());
 
-        Question q2 = new Question();
-        q2.setSubject("스프링 부트 모델 질문입니다.");
-        q2.setContent("id는 자동으로 생성되나요?");
-        q2.setCreateDate(LocalDateTime.now());
-        this.questionRepository.save(q2);
-
+        Question q = all.get(0);
+        assertEquals("sbb가 무엇인가요?", q.getSubject());
     }
-
 }
