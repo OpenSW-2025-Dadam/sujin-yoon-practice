fa5376fa0f4f54ce2254e6624455838b8540eed6
s90d221 <sujin90d@gmail.com>
2025-11-06 01:31:38 +0900

    feat: add SiteUser entity for user registration and login

---
+++ src/main/java/com/mysite/sbb/user/SiteUser.java
@@ -0,0 +1,29 @@
+package com.mysite.sbb.user;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+public class SiteUser {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true, nullable = false, length = 30)
+    private String username;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(unique = true, nullable = false, length = 100)
+    private String email;
+}
